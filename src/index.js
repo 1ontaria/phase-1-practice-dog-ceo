c8b67114bb58bd324bc992309888ad653b1a1932
Ontaria Woods <ontaria.woods@gmail.com>
2023-01-16 15:03:55 -0500

    commit tree e5cfbd240d1bf787c65eb4536ff03eb7480bab96

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
+
+document.addEventListener("DOMContentLoaded", pageLoad());
+
+function pageLoad() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  fetch(imgUrl)
+    .then((response) => response.json())
+    .then((response) => {
+      const dogImageContainer = document.getElementById("dog-image-container");
+      response.message.forEach((url) => {
+        const img = document.createElement("img");
+        img.src = url;
+        dogImageContainer.append(img);
+      });
+    });
+  const breedUrl = "https://dog.ceo/api/breeds/list/all";
+  fetch(breedUrl)
+    .then((response) => response.json())
+    .then((response) => {
+      const ul = document.getElementById("dog-breeds");
+      const keys = Object.keys(response.message);
+      keys.forEach((element) => {
+        const li = document.createElement("li");
+        li.textContent = element;
+        li.addEventListener("click", () => {
+          li.style.color = "pink";
+        });
+        ul.append(li);
+      });
+      const dropdown = document.getElementById("breed-dropdown");
+      keys.filter((letter) => {
+        if (letter === "a") {
+        }
+      });
+    });
+}
